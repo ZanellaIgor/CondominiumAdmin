f9029be3c4d2f81538296320688713fec35233de
ZanellaIgor <igor_zanella@outlook.com>
2024-07-02 23:27:50 -0300

    Instalado react query e configurado app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import ThemeProviderWrapper from '@components/Theme/ThemeProvider';
 import { CssBaseline } from '@mui/material';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useRoutes } from 'react-router-dom';
 import { routes } from './router';
 
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        gcTime: 1000 * 60 * 60 * 24, // 24 hours
+      },
+    },
+  });
   const content = useRoutes(routes);
   return (
     <ThemeProviderWrapper>
-      <CssBaseline />
-      {content}
+      <QueryClientProvider client={queryClient}>
+        <CssBaseline />
+        {content}
+      </QueryClientProvider>
     </ThemeProviderWrapper>
   );
 }
